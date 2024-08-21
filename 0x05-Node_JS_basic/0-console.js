175192053b07090622e41439decad3222370d92f
marky376 <mmutuamark@gmail.com>
2024-08-21 13:32:59 +0300

    Change message writting function

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Mark Munyao <https://github.com/marky376>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
